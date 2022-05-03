f355096c90a7875616adba3d1314619138b8b530
krishnakesa <98970162+krishnakesa@users.noreply.github.com>
2022-05-03 11:25:26 -0400

    Optimized code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as ProductActions from './state/products.actions'
+import * as ProductActions from './state/products.actions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   constructor(private _store: Store) {
+    //First action dispatched
     this._store.dispatch(ProductActions.loadAllProducts());
-
   }
-
 }
